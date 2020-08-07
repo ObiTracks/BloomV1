f625e71b286f2c48c862647a8198ddf383f07f76
ObiTracks <mo.ihej17@gmail.com>
2020-08-07 03:10:29 -0700

    Passed in objects through the view into dashboard html. Work in progress.

--- src/reservations/models.py
+++ src/reservations/models.py
@@ -6,6 +6,7 @@ import datetime, calendar
 
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
+    apt_number = models.IntegerField(default=0, blank=False)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
--- src/reservations/views.py
+++ src/reservations/views.py
@@ -6,17 +6,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import (Customer, Day, TimeSlot, Reservation)
 
 def home_page(request):
-    next_day = Day.objects.last()
-    days = Day.objects.all().count()
-    present_day = Day.objects.last()
-    if days >= 2:
-        present_day = Day.objects.all()[-2]
+    days = Day.objects.all()
+    
+    #Next day Information
+    next_day = days.last()
+    next_day_slots = next_day.timeslot_set.all()
+    # slot_reservations 
+    # timeslots_tomorrow = next_day.timeslot_set()
 
-    timeslots = Day.timeslot_set
+    #Present day information
+    total_days = days.count()
+
+    if total_days >= 2:
+        present_day = days[-2]
+
 
     # total_reservations = timeslots.reservation_set.all().count()
 
-    context = {'next_day':next_day, 'present_day':present_day, "timeslots":timeslots}
+    context = {'days':days, 'next_day':next_day, 'next_day_slots':next_day_slots, 'total_days':total_days}
     template_name = 'home.html'
     
     return render(request, template_name, context)
