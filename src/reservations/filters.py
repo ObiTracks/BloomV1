6b8921ffb2011d7334d2df4997231c82b9ac40de
ObiTracks <mo.ihej17@gmail.com>
2020-09-01 13:44:11 -0400

    Updates to reservation form styling and buttons

--- src/reservations/filters.py
+++ src/reservations/filters.py
@@ -3,14 +3,11 @@ from django_filters import DateFilter, CharFilter
 from .models import *
 
 class CustomerFilter(django_filters.FilterSet):
-    start_date = DateFilter(field_name='date_created', lookup_expr='gte', label='Day after (mm/dd/yyyy):')
-    end_date = DateFilter(field_name='date_created', lookup_expr='lte', label='Day after (mm/dd/yyyy):')
-
     name = CharFilter(field_name='name', lookup_expr='icontains', label='Name')
     apt = CharFilter(field_name='apt', lookup_expr='icontains', label='Apartment')
     phone = CharFilter(field_name='phone', lookup_expr='icontains', label='Phone #')
     email = CharFilter(field_name='email', lookup_expr='icontains', label='Email')
-    notes = CharFilter(field_name='notes', lookup_expr='icontains', label='Notes')
+    day = DateFilter(field_name='date_created', label='Date Created')
 
     class Meta:
         model = Customer
@@ -19,7 +16,7 @@ class CustomerFilter(django_filters.FilterSet):
         
 
 class DayFilter(django_filters.FilterSet):
-    day = DateFilter(field_name='date_created', label='Day', )
+    day = DateFilter(field_name='date_created', label='Day')
     start_date = DateFilter(field_name='date_created', label='Day after (mm/dd/yyyy):', lookup_expr='gte')
     end_date = DateFilter(field_name='date_created', label='Day before (mm/dd/yyyy):', lookup_expr='lte')
 
