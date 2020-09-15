100b22db042eac5e916a0e3bd85ba9eef25f934c
ObiTracks <mo.ihej17@gmail.com>
2020-09-15 14:54:50 -0400

    Customized the user admin panel for increased functionality like searching and displaying other columns of data

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+from account.models import Account
+
+class AccountAdmin(UserAdmin):
+    ordering = ()
+    list_display = ('email','first_name','last_name','apt','is_admin','is_staff')
+    search_fields = ('email','first_name','last_name','apt')
+    readonly_fields = ('date_joined','last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
--- src/reservations/forms.py
+++ src/reservations/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 # from django.contrib.auth.models import User
 from django import forms
 
@@ -21,4 +22,4 @@ class ReservationForm(ModelForm):
 # class CreateUserForm(UserCreationForm):
 #     class Meta:
 #         model = User
-#         fields = ['username','email','password1','password2']
+#         fields = ['first_name','last_name','email','apt']
